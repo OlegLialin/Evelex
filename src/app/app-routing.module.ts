cda37962f544ca7f394dee90908c2367ff6ff823
olegliallin <olegliallin@gmail.com>
2019-04-08 19:21:37 +0300

    comit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {MainComponent} from './main/main.component';
+import {DirectionsComponent} from './directions/directions.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'main', component: MainComponent},
+  {path: 'directions', component: DirectionsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
